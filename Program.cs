a6c3684dffeb8c1436e335d2c87441e571809d0c
Savanna Dickie <personal3@Savannas-MacBook-Pro.local>
2024-09-10 23:23:01 -0600

    finished step: 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+// subtraction
+c = a - b;
+Console.WriteLine(c);
+//multiplication
+c = a * b;
+Console.WriteLine(c);
+//division
+c = a / b;
+Console.WriteLine(c);
--- obj/Debug/net8.0/lab-1-numeric-computation-in-c-SavannaDickie.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-1-numeric-computation-in-c-SavannaDickie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-numeric-computation-in-c-SavannaDickie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e697106b4ddcc5b80d29b6be6c7c14eb8e065a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df5025239779ef3feba23ddcae5e5b36d46af20")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-numeric-computation-in-c-SavannaDickie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-numeric-computation-in-c-SavannaDickie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
